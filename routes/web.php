b2e7149574628b68bc250d0aac989ad1ed7d39d4
YaoTingchen <yaoting0901@gmail.com>
2021-02-23 05:54:56 +0530

    Added individual post page

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::get('/dashboard', [DashboardController::class, 'index'])
     ->name('dashboard');
 
 Route::get('/posts', [PostController::class, 'index'])->name('posts');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::post('/posts', [PostController::class, 'store']);
 Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 
